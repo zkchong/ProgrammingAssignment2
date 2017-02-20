88161ce6e7c5f40cdee95484f5a96a6a2cc30db2
Zan-Kai Chong <zkchong@gmail.com>
2017-02-20 16:17:54 +0800

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+# 
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than computing it repeatedly (there
+# are also alternatives to matrix inversion that we will not discuss here). Your
+# assignment is to write a pair of functions that cache the inverse of a matrix.
+# 
+# Write the following functions:
+# 
+# makeCacheMatrix: 
+# This function creates a special "matrix" object that can cache its inverse. 
+#
+# cacheSolve: 
+# This function computes the inverse of the special "matrix" returned by
+# makeCacheMatrix above. If the inverse has already been calculated (and the
+# matrix has not changed), then cacheSolve should retrieve the inverse from the
+# cache. Computing the inverse of a square matrix can be done with the solve
+# function in R. For example, if X is a square invertible matrix, then solve(X)
+# returns its inverse.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+    #
+    inv <- NULL
+    #
+    set <- function(y) {  # set the matrix
+        x <<- y
+        inv <<- NULL
+    }
+    #
+    get <- function() x # get the matrix
+    
+    # Set the inverse
+    setInverse <- function(inverse) inv <<- inverse
+    
+    # Get the inverse
+    getInverse <- function() inv
+    
+    #
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    # If the inverse is not pre-calculated, we calculate it for the first time now. 
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
